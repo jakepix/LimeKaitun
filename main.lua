bfac7149a1199478f6e8a0ea77578e66dae278bc
jakepix <hollandesevene10@gmail.com>
2025-12-26 07:14:52 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,14 @@
 _G.AutoFarm = true
 _G.Team = "Marines" -- Escolha automática de time
 
--- LINKS RAW DO SEU GITHUB (Substitua quando criar os arquivos)
+-- LINKS RAW DO SEU GITHUB
 local Links = {
+    Data = "https://raw.githubusercontent.com/jakepix/LimeKaitun/refs/heads/main/data.lua",
+    Funcs = "https://raw.githubusercontent.com/jakepix/LimeKaitun/refs/heads/main/funcs.lua",
+    Frontend = "https://raw.githubusercontent.com/jakepix/LimeKaitun/refs/heads/main/frontend.lua",
+    Tasks = "https://raw.githubusercontent.com/jakepix/LimeKaitun/refs/heads/main/tasks.lua"
+} -- O erro estava aqui, faltava fechar a chave!
+
 local Data = loadstring(game:HttpGet(Links.Data))()
 local Funcs = loadstring(game:HttpGet(Links.Funcs))()
 local UI = loadstring(game:HttpGet(Links.Frontend))()
@@ -10,7 +16,7 @@ local Tasks = loadstring(game:HttpGet(Links.Tasks))()
 
 UI.Init()
 
--- Loop de 0.03s conforme solicitado
+-- Loop de 0.02s conforme solicitado
 task.spawn(function()
     while task.wait(0.02) do
         if _G.AutoFarm then
@@ -22,12 +28,14 @@ end)
 -- Otimização de CPU/GPU para Kaitun
 if not game:IsLoaded() then game.Loaded:Wait() end
 settings().Rendering.DrawDistanceMax = 150
-game:GetService("RunService"):Set3dRenderingEnabled(false) -- Desativa o 3D (FPS sobe muito)
 
--- Anti-AFK para não cair da conta
+-- Se quiser ver a UI, coloque -- antes da linha abaixo
+-- game:GetService("RunService"):Set3dRenderingEnabled(false)
+
+-- Anti-AFK
+local player = game.Players.LocalPlayer
 player.Idled:Connect(function()
     game:GetService("VirtualUser"):Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
     task.wait(1)
     game:GetService("VirtualUser"):Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
-
 end)
